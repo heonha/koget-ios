6028a7ab40dc1a8c1f80a6133461e78d2a5c8157
weizen511 <weizen511@gmail.com>
2022-11-25 00:48:18 +0900

    정리 : 불필요한 코드 정리

--- BackgroundMaker/WidgetMaker/deepLinkWidget/Models/WidgetModels.swift
+++ BackgroundMaker/WidgetMaker/deepLinkWidget/Models/WidgetModels.swift
@@ -5,14 +5,10 @@
 //  Created by HeonJin Ha on 2022/11/23.
 //
 
-import UIImageColors
-import UIKit
 import SwiftUI
-import RxSwift
 
 final class WidgetModels: ObservableObject {
     
-    @Published var widgets: [DeepLink] = WidgetCoreData.shared.widgets.value
     @Published var widgetName: String = ""
     @Published var widgetURL: String = ""
     @Published var widgetImage: UIImage?
