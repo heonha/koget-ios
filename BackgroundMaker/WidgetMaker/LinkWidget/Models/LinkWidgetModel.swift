e38bb896e707908763728909c490f70c88c72fb7
weizen511 <weizen511@gmail.com>
2022-12-01 17:59:43 +0900

    디버깅: 잠금화면에서 딥링크 위젯 이미지를 불러올 수 없는 현상 수정

--- BackgroundMaker/WidgetMaker/LinkWidget/Models/LinkWidgetModel.swift
+++ BackgroundMaker/WidgetMaker/LinkWidget/Models/LinkWidgetModel.swift
@@ -7,18 +7,57 @@
 
 import SwiftUI
 
+
+
 final class LinkWidgetModel: ObservableObject {
     
-    @Published var widgetName: String = ""
-    @Published var widgetURL: String = ""
-    @Published var widgetImage: UIImage?
+    @Published var name: String = ""
+    @Published var url: String = ""
+    @Published var image: UIImage?
         
     func getWidgetData(selectedWidget: LinkWidget) {
-        self.widgetName = selectedWidget.appNameGlobal
-        self.widgetURL = selectedWidget.url
-        self.widgetImage = selectedWidget.image!
+        self.name = selectedWidget.appNameGlobal
+        self.url = selectedWidget.url
+        self.image = selectedWidget.image!
     }
     
 
     
+}
+
+enum LinkType {
+    case inApp
+    case custom
+}
+
+/// DeepLinking 할 앱의 정보
+struct LinkWidget {
+    
+    // DB 데이터
+    let id = UUID()
+    let type: LinkType = .inApp
+    var appName: String
+    var appNameGlobal: String
+    var url: String
+    var imageName: String
+
+    // 후가공 데이터
+    var displayName: String?
+    var image: UIImage?
+    
+    mutating func getImage(imageName: String) {
+        self.image = UIImage(named: imageName)
+    }
+    
+    mutating func getName() {
+        let langStr = Locale.current.language.languageCode?.identifier
+        
+        if langStr == "ko" { // 한국어라면 글로벌 네임을 보여줘라.
+            if appName == appNameGlobal {
+                self.displayName = appName
+            } else {
+                self.displayName = "\(appName) (\(appNameGlobal))"
+            }
+        }
+    }
 }
