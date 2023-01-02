372774a4b5b98f0fab7c4464fb503c75e984a506
weizen511 <weizen511@gmail.com>
2023-01-02 22:00:46 +0900

    STYLE: 위젯 편집버튼 눌림 효과 없이 전환되도록 변경

--- QuickGet/AppDelegate.swift
+++ QuickGet/AppDelegate.swift
@@ -45,6 +45,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
     
-    
-    
 }
