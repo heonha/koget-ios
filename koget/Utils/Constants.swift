a99b8b88d16bf036a15015d30b07935704fa5af2
weizen511 <weizen511@gmail.com>
2023-01-08 16:29:02 +0900

    CoreData Entity 및 Coredata xcdatamodel 파일 이름 변경

--- koget/Utils/Constants.swift
+++ koget/Utils/Constants.swift
@@ -17,7 +17,7 @@ struct Constants {
     //MARK: - 공통 변수
     //MARK: - Widget Constants
 
-    static let coreDataEntityName = "QuickGet"
+    static let coreDataEntityName = "WidgetModel"
     static let appGroupID = "group.ZH5GA3W8UP.com.heon.koget"
     
 }
