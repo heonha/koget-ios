fee2b34bf051e9e72a0f2f7d98e88d5a633b4510
weizen511 <weizen511@gmail.com>
2023-01-10 20:40:18 +0900

    SPM Firebase 추가

--- koget/SceneDelegate.swift
+++ koget/SceneDelegate.swift
@@ -6,12 +6,15 @@
 //
 
 import SwiftUI
+import FirebaseCore
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     
     var window: UIWindow?
     
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        
+        FirebaseApp.configure()
         
         
         guard let windowScene = (scene as? UIWindowScene) else { return }
