0e6563302fe4263578453406640506d26c1e381d
HeonJin Ha <heonapp@gmail.com>
2023-06-27 12:58:38 +0900

    refactor: 리스트셀 기본 스타일 작업

--- koget/UI/NewHome/ListCell.swift
+++ koget/UI/NewHome/ListCell.swift
@@ -11,9 +11,18 @@ struct ListCell: View {
     
     @State private var widget: DeepLink
     @State private var offsetX: CGFloat = .zero
+    @State private var widgetIcon: UIImage = UIImage()
     
     init(widget: DeepLink) {
         self.widget = widget
+    }
+    
+    private func setIcon() {
+        if let data = widget.image, let image = UIImage(data: data) {
+            self.widgetIcon = image
+        } else {
+            self.widgetIcon = UIImage(systemName: "person")!
+        }
     }
     
     var body: some View {
@@ -22,8 +31,7 @@ struct ListCell: View {
                 .fill(Color(hex: "F9F9F9"))
             
             HStack {
-                Image
-                    .uiImage(UIImage(data: widget.image ?? Data()) ?? UIImage())
+                Image(uiImage: $widgetIcon.wrappedValue)
                     .resizable()
                     .scaledToFit()
                     .frame(width: 40, height: 40)
@@ -31,16 +39,19 @@ struct ListCell: View {
                     .clipShape(Circle())
                     .shadow(color: .black.opacity(0.18), radius: 4, x: 0.3, y: 0.3)
                     .padding(.leading)
+                    .onAppear {
+                        setIcon()
+                    }
                 
                 VStack(alignment: .leading, spacing: 4) {
                     Text(widget.name ?? "")
                         .font(.custom(.robotoBold, size: 15))
                     
-                    
                     Text(widget.type ?? "앱")
                         .font(.custom(.robotoMedium, size: 13))
                         .foregroundColor(Color.init(hex: "85858B"))
                 }
+                
                 Spacer()
                 
                 Capsule()
@@ -54,7 +65,6 @@ struct ListCell: View {
                             
                             Text("100")
                                 .font(.custom(.robotoMedium, size: 14))
-                            
                         }
 
                     }
@@ -62,31 +72,23 @@ struct ListCell: View {
                     
             }
         }
-        .offset(x: offsetX)
         .cornerRadius(8)
         .shadow(color: .black.opacity(0.20), radius: 2, x: 0.3, y: 0.3)
         .frame(height: 58)
         .padding(.horizontal, 2)
+        .offset(x: offsetX)
         .gesture(
             DragGesture(minimumDistance: 30)
                 .onChanged { value in
                     let changedOffset = value.translation.width
-                        withAnimation(.spring()) {
-                            offsetX = changedOffset
-                        }
-                    print(changedOffset)
+                    offsetX = changedOffset
                 }
                 .onEnded { value in
                     if value.translation.width < -70 {
-                        withAnimation(.spring()) {
-                            offsetX = -150
-                        }
-                    } else {
-                        withAnimation(.spring()) {
-                            offsetX = .zero
-                        }
-                    }
-                    
+                             offsetX = -150
+                     } else {
+                             offsetX = .zero
+                     }
                 }
         )
         .gesture(
@@ -96,13 +98,14 @@ struct ListCell: View {
                 }
                 .onEnded { value in
                     print("DEBUG: LongPress Ended")
-//                    HapticManager.shared.triggerHapticFeedback(style: .heavy)
+                    HapticManager.shared.triggerHapticFeedback(style: .heavy)
 //
 //                    if let url = widget.url, let id = widget.id {
 //                        viewModel.urlOpenedInApp(urlString: "\(WidgetConstant.mainURL)\(url)\(WidgetConstant.idSeparator)\(id.uuidString)")
 //                    }
                 }
         )
+        .animation(.spring(response: 0.5, dampingFraction: 1, blendDuration: 0.7), value: offsetX)
     }
 }
 
